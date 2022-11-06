d9f79c9976e81a80a66adc495969b4b587d0470e
ueno-8-ueno <usus.kobaryuji0808@gmail.com>
2022-11-06 16:43:37 +0900

    ワークスペース閉じた後に、再び立ち上げてjavaのみ変更

--- GithubTest.java
+++ GithubTest.java
@@ -3,5 +3,6 @@ public class GithubTest{
         System.out.println("テスト");
         System.out.println("push");
         System.out.println("javaのみ変更");
+        System.out.prntln("ワークスペース閉じた後に変更");
     }
 }
