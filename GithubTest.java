0bcea1599d3098039f20c6383669ea375074d800
ueno-8-ueno <usus.kobaryuji0808@gmail.com>
2022-11-06 16:02:50 +0900

    javaのみ変更

--- GithubTest.java
+++ GithubTest.java
@@ -2,5 +2,6 @@ public class GithubTest{
     public static void main(String[] args){
         System.out.println("テスト");
         System.out.println("push");
+        System.out.println("javaのみ変更");
     }
 }
