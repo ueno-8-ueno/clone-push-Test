c532db5a69c5eaf2a2f722739596fdc73cbb7134
ueno-8-ueno <usus.kobaryuji0808@gmail.com>
2022-12-08 20:19:15 +0900

    間違えた実装(あとで消す)

--- GithubTest.java
+++ GithubTest.java
@@ -10,3 +10,8 @@ public class GithubTest{
 public void createBranch(){
     System.out.println("ブランチを作成後、コードを追記しました。");
 }
+
+public int missMethod(){
+    System.out.println("間違えた実装をしてしまった");
+    return -1;
+}
